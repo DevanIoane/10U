3f4765ed55c1a2ca2fa4e47e5842c4b136b6f58f
DevanIoane <161520230+DevanIoane@users.noreply.github.com>
2024-03-08 12:06:32 +1300

    Variables

---
+++ P_O2_Variables.py
@@ -0,0 +1,13 @@
+# Code to Learn about variables...
+
+# Variables can be a string (note the speech marks)
+greetings = "hello world"
+
+# Variables can be a number
+fav_num = 7
+num_stirng * "5"
+# Constants are uppercase
+PI = 3.14
+
+
+a_01_My_num = 7
